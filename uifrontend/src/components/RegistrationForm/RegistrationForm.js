7695485bf8912447c0ba7ffe793d740e98d7930f
ayishacs <ayishatusaa@gmail.com>
2021-02-19 11:41:13 -0500

    added state variables to update ui elements

--- uifrontend/src/components/RegistrationForm/RegistrationForm.js
+++ uifrontend/src/components/RegistrationForm/RegistrationForm.js
@@ -1,8 +1,68 @@
 import React, {useState} from 'react';
+import axios from 'axios';
 import './RegistrationForm.css';
+import {API_BASE_URL, ACCESS_TOKEN_NAME} from '../../constants/apiConstants';
 import { withRouter } from "react-router-dom";
 
-function RegistrationForm(){
+function RegistrationForm(props) {
+    const [state , setState] = useState({
+        email : "",
+        password : "",
+        confirmPassword: "",
+        successMessage: null
+    })
+    const handleChange = (e) => {
+        const {id , value} = e.target   
+        setState(prevState => ({
+            ...prevState,
+            [id] : value
+        }))
+    }
+    const sendDetailsToServer = () => {
+        if(state.email.length && state.password.length) {
+            props.showError(null);
+            const payload={
+                "email":state.email,
+                "password":state.password,
+            }
+            axios.post(API_BASE_URL+'/user/register', payload)
+                .then(function (response) {
+                    if(response.status === 200){
+                        setState(prevState => ({
+                            ...prevState,
+                            'successMessage' : 'Registration successful. Redirecting to home page..'
+                        }))
+                        localStorage.setItem(ACCESS_TOKEN_NAME,response.data.token);
+                        redirectToHome();
+                        props.showError(null)
+                    } else{
+                        props.showError("Some error ocurred");
+                    }
+                })
+                .catch(function (error) {
+                    console.log(error);
+                });    
+        } else {
+            props.showError('Please enter valid username and password')    
+        }
+        
+    }
+    const redirectToHome = () => {
+        props.updateTitle('Home')
+        props.history.push('/home');
+    }
+    const redirectToLogin = () => {
+        props.updateTitle('Login')
+        props.history.push('/login'); 
+    }
+    const handleSubmitClick = (e) => {
+        e.preventDefault();
+        if(state.password === state.confirmPassword) {
+            sendDetailsToServer()    
+        } else {
+            props.showError('Passwords do not match');
+        }
+    }
     return(
         <div className="card col-12 col-lg-4 login-card mt-2 hv-center">
             <form>
