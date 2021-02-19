063cd146806410b8254804004bbef7d88b448a85
suraj <surajpunjabi28@gmail.com>
2021-02-18 21:45:00 -0500

    Adding login and sign up pages

--- panorama-ui/react-ui/src/index.js
+++ panorama-ui/react-ui/src/index.js
@@ -6,16 +6,15 @@ import '../public/design/bootstrap/css/bootstrap.min.css';
 import Navigation from './components/Navbar';
 import Routes from './Routes';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { BrowserRouter} from "react-router-dom"
 
 ReactDOM.render(
   <React.StrictMode>
+    <BrowserRouter>
     <App />
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
