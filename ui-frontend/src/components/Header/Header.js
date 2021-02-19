9f1c9eff548c8c936168ea114956128ca3946d03
ayishacs <ayishatusaa@gmail.com>
2021-02-19 11:30:48 -0500

    node modules deleted to implement CDN

---
+++ ui-frontend/src/components/Header/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Header.css';
+
+function Header() {
+    return(
+        <nav class="navbar navbar-dark bg-dark">
+            <div className="row col-12 d-flex justify-content-center text-white">
+            <span className="h3">Panorama - Memories For Life</span>
+            </div>
+        </nav>
+    )
+}
+export default Header;
---
+++ ui-frontend/src/components/RegistrationForm/RegistrationForm.js
@@ -0,0 +1,61 @@
+import React, {useState} from 'react';
+import './RegistrationForm.css';
+import { withRouter } from "react-router-dom";
+
+function RegistrationForm(){
+    return(
+        <div className="card col-12 col-lg-4 login-card mt-2 hv-center">
+            <form>
+                <div className="form-group text-left">
+                <label htmlFor="exampleInputEmail1">Email address</label>
+                <input type="email" 
+                       className="form-control" 
+                       id="email" 
+                       aria-describedby="emailHelp" 
+                       placeholder="Enter Email" 
+                       value={state.email}
+                       onChange={handleChange}
+                />
+                </div>
+                <div className="form-group text-left">
+                    <label htmlFor="exampleInputPassword1">Password</label>
+                    <input type="password" 
+                        className="form-control" 
+                        id="password" 
+                        placeholder="Password"
+                        value={state.password}
+                        onChange={handleChange} 
+                    />
+                </div>
+                <div className="form-group text-left">
+                    <label htmlFor="exampleInputPassword1">Confirm Password</label>
+                    <input type="password" 
+                        className="form-control" 
+                        id="confirmPassword" 
+                        placeholder="Confirm Password"
+                        value={state.confirmPassword}
+                        onChange={handleChange} 
+                    />
+                </div>
+                <button className="buttonRegister"
+                    type="submit" 
+                  //  className="btn btn-primary"
+                    class="btn btn-danger"
+                    onClick={handleSubmitClick}
+                >
+                    Register
+                </button>
+            </form>
+            <div className="alert alert-success mt-2" style={{display: state.successMessage ? 'block' : 'none' }} role="alert">
+                {state.successMessage}
+            </div>
+            <div className="mt-2">
+                <span>Already have an account? </span>
+                <span className="loginText" onClick={() => redirectToLogin()}>Login here</span> 
+            </div>
+            
+        </div>
+    )
+}
+
+export default withRouter(RegistrationForm);
