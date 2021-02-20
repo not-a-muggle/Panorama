498a543eacb35b4f6628106159c5cd8b98cbbccd
suraj <surajpunjabi28@gmail.com>
2021-02-20 15:07:52 -0500

    Cleaning the code

--- panorama-ui/react-ui/src/index.js
+++ panorama-ui/react-ui/src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import '../public/design/bootstrap/css/bootstrap.min.css';
 import App from './App';
 import { BrowserRouter} from "react-router-dom"
 
