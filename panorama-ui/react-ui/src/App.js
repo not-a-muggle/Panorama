c9dec42b76a4ee3e545810dff5a25ab440c67b09
ayishacs <ayishatusaa@gmail.com>
2021-02-20 16:04:25 -0500

    updated components

--- panorama-ui/react-ui/src/App.js
+++ panorama-ui/react-ui/src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
-import Home from './compnents/Home'
-import SignIn from './compnents/SignIn'
-import SignUp from './compnents/SignUp'
-import Error from './compnents/Error'
-import Navbar from './compnents/Navbar'
+import Home from './components/Home'
+import SignIn from './components/SignIn'
+import SignUp from './components/SignUp'
+import Error from './components/Error'
+import Navbar from './components/Navbar'
+import Album from './components/Album'
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
 
 class App extends React.Component {
@@ -16,6 +17,7 @@ class App extends React.Component {
                 <Route path="/" component={Home} exact />
                 <Route path="/SignIn" component={SignIn} />
                 <Route path="/SignUp" component={SignUp} />
+                <Route path="/Album" component={Album} />
                 <Route component={Error} />
             </Switch>
     </Router>
