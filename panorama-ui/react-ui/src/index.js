9b003c55fd54632d0db56e175bc7a541a8f27dc5
ayishacs <ayishatusaa@gmail.com>
2021-02-20 15:01:06 -0500

    modified the file content to align with changes

--- panorama-ui/react-ui/src/index.js
+++ panorama-ui/react-ui/src/index.js
@@ -1,10 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router } from 'react-router-dom';
 import './index.css';
 import '../public/design/bootstrap/css/bootstrap.min.css';
-import Navigation from './components/Navbar';
-import Routes from './Routes';
 import App from './App';
 import { BrowserRouter} from "react-router-dom"
 
