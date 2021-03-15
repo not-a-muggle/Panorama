cd0956d750de4e9f3ec64da124b08eac8aee7992
ayishacs <ayishatusaa@gmail.com>
2021-03-15 18:46:27 -0400

    updated script for jenkins build

--- panorama-ui/react-ui/build.sh
+++ panorama-ui/react-ui/build.sh
@@ -2,5 +2,5 @@
 cd /var/lib/jenkins/workspace/Build_ReactUI/panorama-ui/react-ui
 chmod 755 build.sh
 echo "Building dockerfile for react-ui"
-echo qwerty | sudo -S docker build -t  react-ui:v7 .
-echo qwerty | sudo -S docker run -p 8004:80 react-ui:v7
+echo qwerty | sudo -S docker build -t  react-ui:v8 .
+echo qwerty | sudo -S docker run -p 8004:80 react-ui:v8
