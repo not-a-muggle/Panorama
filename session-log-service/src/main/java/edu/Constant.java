0724de0b462e34b677555ae7edb2b1e2ecbc3dcc
Vishesh Dembla <vdembla@iu.edu>
2021-03-22 15:38:26 -0400

    #149 dockerfile for session-log-service

---
+++ session-log-service/src/main/java/edu/Constant.java
@@ -0,0 +1,6 @@
+package edu;
+
+public class Constant {
+    public static final String COLLECTION_NAME = "session_log";
+    public static final String DATABASE_NAME = "panorama";
+}
---
+++ session-log-service/src/main/java/edu/DatabaseClient.java
@@ -0,0 +1,72 @@
+package edu;
+
+import com.mongodb.BasicDBObject;
+import com.mongodb.MongoClient;
+import com.mongodb.client.FindIterable;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import org.bson.Document;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class DatabaseClient {
+
+    private static DatabaseClient instance;
+
+    private MongoClient mongoClient;
+    private MongoDatabase database;
+    private MongoCollection<Document> sessionLogCollection;
+
+    private DatabaseClient() {
+        mongoClient = connect();
+        database = mongoClient.getDatabase(Constant.DATABASE_NAME);
+        sessionLogCollection = database.getCollection(Constant.COLLECTION_NAME);
+    }
+
+    public static DatabaseClient getInstance() {
+        if (instance == null) {
+            instance = new DatabaseClient();
+        }
+        return instance;
+    }
+
+    public void insertActivity(String userId, String sessionId, String activityDesc, String time) {
+        Document activity = new Document("userId", userId)
+                .append("sessionId", sessionId)
+                .append("activityDesc", activityDesc)
+                .append("time", time);
+        sessionLogCollection.insertOne(activity);
+    }
+
+    public SessionLog.Activity[] fetchActivities(String userId, String sessionId) {
+        System.out.println("here");
+        BasicDBObject searchQuery = new BasicDBObject();
+        searchQuery.put("userId", userId);
+        searchQuery.put("sessionId", sessionId);
+        FindIterable<Document> cursor = sessionLogCollection.find(searchQuery);
+
+        Iterator<Document> iterator = cursor.iterator();
+        ArrayList<SessionLog.Activity> activities = new ArrayList<>();
+
+        while (iterator.hasNext()) {
+            Document doc = iterator.next();
+            String activityDesc = doc.get("activityDesc").toString();
+            String time = doc.get("time").toString();
+
+            SessionLog.Activity activity = SessionLog.Activity.newBuilder().
+                    setTime(time).
+                    setActivityDesc(activityDesc).
+                    setSessionId(sessionId).
+                    setUserId(userId).build();
+
+            activities.add(activity);
+        }
+        return (SessionLog.Activity[]) activities.toArray();
+    }
+
+    private MongoClient connect() {
+        return new MongoClient("localhost", 27017);
+    }
+
+}
