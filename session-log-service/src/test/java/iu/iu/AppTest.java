d693c6b70cd24d6fb6a72720296ca2499ce0f440
suraj <surajpunjabi28@gmail.com>
2021-03-24 14:41:26 -0400

    Adding test files

--- session-log-service/src/test/java/iu/iu/AppTest.java
+++ session-log-service/src/test/java/iu/iu/AppTest.java
@@ -1,20 +1,31 @@
 package iu.iu;
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
 
-/**
- * Unit test for simple App.
- */
+import edu.DatabaseClient;
+import edu.SessionLog;
+
+
+
+
 public class AppTest 
 {
-    /**
-     * Rigorous Test :-)
-     */
     @Test
     public void shouldAnswerWithTrue()
     {
-        assertTrue( true );
+        String userId = "1234";
+        String sessionId = "Session123";
+        String activityDesc = "Activity";
+        String time = "1616551403";
+        DatabaseClient databaseClient = DatabaseClient.getInstance();
+
+        databaseClient.insertActivity(userId, sessionId, activityDesc, time);
+        SessionLog.Activity activities[] = databaseClient.fetchActivities(userId, sessionId);
+        String fetchedActivity = activities[0].getActivityDesc();
+        String fetchedTime = activities[0].getTime();
+        assertEquals(activityDesc, fetchedActivity );
+        assertEquals(time, fetchedTime);
     }
 }
