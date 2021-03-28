b9c56e7214136493e515fe2f6d94da5f03f50819
ayishacs <ayishatusaa@gmail.com>
2021-03-28 11:38:11 -0400

    Restructured cd branch for session

--- user-service/src/test/DatabaseClient.test.ts
+++
@@ -1,8 +0,0 @@
-import DatabaseClient from "../DatabaseClient"
-import * as chai from "chai";
-describe("test cases", ()=> {
-    it("Conection to Db", ()=> {
-        const db = DatabaseClient.Instance;
-        chai.assert.isTrue(!db.client.isConnected());
-    })
-})
