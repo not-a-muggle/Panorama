d2c1fc7101c996a87dec0a09bbd236c805a1a01c
vishesh <visheshdembla@hotmail.com>
2021-04-15 18:04:13 -0400

    server changes

--- image-service/server.py
+++ image-service/server.py
@@ -14,7 +14,7 @@ server = grpc.server(futures.ThreadPoolExecutor(max_workers=10), options=[
 
 images_pb2_grpc.add_ImageServiceServicer_to_server(
     images_pb2_grpc.ImageServiceServicer(), server)
-server.add_insecure_port('127.0.0.1:30300')
+server.add_insecure_port('0.0.0.0:30300')
 server.start()
 print("Image Server Started on port 30300")
 logging.info("server started on port 30300")
