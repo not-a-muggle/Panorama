89075bb100eee36bf17948d0ed58aca882dd0126
Vishesh Dembla <vdembla@iu.edu>
2021-04-28 01:37:38 -0400

    #206 unit test case added

--- share-service/src/test/share.test.ts
+++ share-service/src/test/share.test.ts
@@ -1,5 +1,13 @@
 import ShareServiceCore from "../core/Share";
 
-const share: ShareServiceCore = new ShareServiceCore();
-// share.shareImages('123','345',['123','124','125']);
-share.unshareImages('123','345', ['123','124','125']);
+describe('Share Unshare ', async () => {
+    it("share", async () => {
+        const share: ShareServiceCore = new ShareServiceCore();
+        await share.shareImages('123', '345', ['123', '124', '125']);
+
+    });
+    it("unshare", async () => {
+        const share: ShareServiceCore = new ShareServiceCore();
+        await share.unshareImages('123', '345', ['123', '124', '125']);
+    })
+})
